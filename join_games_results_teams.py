3bc904251ab991ab84f862d1481346ccde6c5a87
Milena-Zakaryan <147372354+Milena-Zakaryan@users.noreply.github.com>
2025-01-07 14:06:42 +0400

    Create join_games_results_teams.py

---
+++ join_games_results_teams.py
@@ -0,0 +1,15 @@
+--SELECT igra.nazvanie AS igra_name, komanda.nazvanie AS komanda_name, rezultati.balli 
+--FROM rezultati
+--JOIN komanda ON rezultati.komanda_id = komanda.id
+--JOIN igra ON rezultati.igra_id = igra.id;
+
+
+@app.route('/results/with-details', methods=['GET'])
+def results_with_details():
+    results = db.session.execute("""
+        SELECT igra.nazvanie AS igra_name, komanda.nazvanie AS komanda_name, rezultati.balli 
+        FROM rezultati
+        JOIN komanda ON rezultati.komanda_id = komanda.id
+        JOIN igra ON rezultati.igra_id = igra.id
+    """).fetchall()
+    return jsonify([dict(row) for row in results])
