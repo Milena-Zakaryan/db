5245030293f6b3294bd58ff6065065e3d23d90cd
Milena-Zakaryan <147372354+Milena-Zakaryan@users.noreply.github.com>
2025-01-07 14:05:39 +0400

    Create select_with_multiple_conditions.py

---
+++ select_with_multiple_conditions.py
@@ -0,0 +1,12 @@
+--SELECT * FROM komanda WHERE gorod = 'City A' AND vuz = 'University X';
+
+@app.route('/teams/filter', methods=['GET'])
+def filter_teams():
+    gorod = request.args.get('gorod')
+    vuz = request.args.get('vuz')
+    if not gorod or not vuz:
+        return jsonify({"error": "gorod and vuz are required parameters"}), 400
+    teams = Team.query.filter_by(gorod=gorod, vuz=vuz).all()
+    return jsonify([team.as_dict() for team in teams])
+
+
